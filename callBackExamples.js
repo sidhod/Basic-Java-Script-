85ef249e506a603936eb51256f9967c1ff56eec7
unknown <sidhodhank2018@gmail.com>
2022-11-10 21:54:26 +0530

    Add:Example No 3 Of Call Back Function

--- callBackExamples.js
+++ callBackExamples.js
@@ -24,3 +24,27 @@ function lastname(lastName) {
 
 // calling the function after 2 seconds
 setTimeout(firstname, 2000, "Sidhodhan", "Kamble", lastname);
+
+// Callback Function Example No 3
+function checkUserName(userName, password, checkPassword) {
+    if (userName == "Sidhodhan") {
+        console.log("Correct User name");
+        // callback function
+        checkPassword(password);
+    } else {
+        console.log("Invalid User name")
+    }
+
+}
+
+// callback function
+function checkPassword(password) {
+    if (password == "Pass@123") {
+        console.log("Correct Password");
+        console.log("Login successful");
+    }
+}
+
+// calling the function after 2 seconds
+//Pass username password and callbackfunction
+setTimeout(checkUserName, 2000, "Sidhodhan", "Pass@123", checkPassword);
