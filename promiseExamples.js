a729484b3da9d15649311eb7358a35bf88ceecb8
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:06:13 +0530

    Add:Examples No 4 Of Promise

--- promiseExamples.js
+++ promiseExamples.js
@@ -59,3 +59,36 @@ let demo2 = () => {
 demo2()
     .then((value) => console.log("Hi guys, My name is" + value))
     .catch((error) => console.log("Invalid", error));
+
+
+// Example of Promise No 4
+function display(firstName, lastName) {
+    console.log("My Name is " + firstName + " " + lastName)
+
+}
+function error1() {
+    return "First Name is empty";
+}
+function error2() {
+    return "Last Name is empty";
+}
+let demo3 = () => {
+    return new Promise((resolve, reject) => {
+        const prompt = require('prompt-sync')();
+        let firstName = prompt("Enter Your Fist Name: ");
+        let lastName = prompt("Enter Your Last Name: ");
+        if (firstName == "") {
+            reject(error1());
+        }
+        else if (lastName == "") {
+            reject(error2());
+        } else {
+            resolve(display(firstName, lastName));
+
+        }
+
+    });
+}
+demo3()
+    .then()
+    .catch((error) => console.log("Invalid", error));
