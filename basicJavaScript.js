9ad4c43a853f16bc52fc4d0ea29dc5865d28ab46
unknown <sidhodhank2018@gmail.com>
2022-11-10 21:39:35 +0530

    Add: Arrow Function To Add Three Numbers

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -7,3 +7,14 @@ function add(a, b, c) {
     console.log("normal function Addition of three no:", add);
 }
 add(10, 10, 20);
+
+//anoynomus function
+//(c,d)=>{
+//     console.log("values",c,d);
+// }
+
+//Arrow Functions
+let addi = (a, b, c) => {
+    console.log("arrow function addition", a + b + c);
+}
+addi(10, 20, 30);
