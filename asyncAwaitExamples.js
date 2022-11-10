5a43561082cf938ff557742703f559e8860a906e
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:18:52 +0530

    Add:Examples No 5 Of Async Await

--- asyncAwaitExamples.js
+++ asyncAwaitExamples.js
@@ -127,3 +127,37 @@ async function asyncCall3() {
 }
 //Call Function
 asyncCall3();
+
+// Example of Async Await No 5
+const employeeData = [];
+function insert(name, message, age) {
+
+    employeeData.push(name);
+    employeeData.push(message);
+    employeeData.push(age);
+}
+
+function insertInArray(name, mobileNumber, age) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve((insert(name, mobileNumber, age)));
+        }, 3000);
+    });
+}
+
+//async fuction
+async function asyncCall4() {
+    console.log("Welcome");
+    const prompt = require('prompt-sync')();
+    let name = prompt("Enter employee name: ");
+    let mobileNumber = prompt("Enter employee Mobile Number: ");
+    let age = prompt("Enter employee Age: ");
+    console.log("--Data is Added To database--");
+    console.log("wait...")
+    await insertInArray(name, mobileNumber, age);
+    console.log("--Data Inserted--");
+    console.log(employeeData);
+
+}
+//Call Function
+asyncCall4();
