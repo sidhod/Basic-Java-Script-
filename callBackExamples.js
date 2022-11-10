74714f8e48676f6c162450efb6a2d0f557a4da60
unknown <sidhodhank2018@gmail.com>
2022-11-10 21:56:15 +0530

    Add:Example No 5 Of Call Back Function

--- callBackExamples.js
+++ callBackExamples.js
@@ -59,4 +59,30 @@ const add = function (a, b, callback) {
 function sum(a, b) {
     console.log("addition:", a + b);
 }
-add(1, 2, sum)
+add(1, 2, sum);
+
+
+// Callback Function Example No 5
+function printMobileNumber(mobileNumber, otp, checkOtp) {
+    console.log("Mobile Number: " + mobileNumber);
+    // callback function
+    checkOtp(otp);
+}
+
+// callback function
+function checkOtp(otp) {
+    const prompt = require('prompt-sync')();
+    let inputOtp = prompt("Enter Otp You Recived: ");
+    if (inputOtp == otp) {
+        console.log("Sucessful Validation!!!");
+    } else {
+        console.log("Unsucessful Validation!!!");
+        console.log("Otp genrated: " + otp);
+        console.log("Otp You enter: " + inputOtp);
+    }
+}
+let mobileNumber = "9309951100";
+let otp = Math.floor(Math.random() * 7000) + 1000;
+// calling the function after 2 seconds
+//Otp Validation pass parameter mobileNumber And random otp
+setTimeout(printMobileNumber, 5000, mobileNumber, otp, checkOtp);
