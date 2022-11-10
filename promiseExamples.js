e08139d279a75bd1960342147fea650eee470d9a
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:02:14 +0530

    Add:Examples No 1 Of Promise

---
+++ promiseExamples.js
@@ -0,0 +1,14 @@
+// Example of Promise No 1
+const demo = () => {
+    return new Promise((resolve, reject) => {
+        let flag = true;
+        if (flag == true) {
+            resolve("validate")
+        } else {
+            reject("Invalidate");
+        }
+    });
+}
+demo()
+    .then((value) => console.log("after resolve-->", value))
+    .catch((error) => console.log("after reject-->", error));
