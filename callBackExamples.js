27ef4026c71d2d1bb3b0a638b7b8adb84385f851
unknown <sidhodhank2018@gmail.com>
2022-11-10 21:53:38 +0530

    Add:Example No 2 Of Call Back Function

--- callBackExamples.js
+++ callBackExamples.js
@@ -9,3 +9,18 @@ function myCalculator(num1, num2, myCallback) {
 }
 
 myCalculator(5, 5, myDisplayer);
+
+// Callback Function Example No 2
+function firstname(firstName, lastName, myFunction) {
+    console.log("First Name:", firstName);
+    // callback function
+    lastname(lastName);
+}
+
+// callback function
+function lastname(lastName) {
+    console.log("Last Name: ", lastName);
+}
+
+// calling the function after 2 seconds
+setTimeout(firstname, 2000, "Sidhodhan", "Kamble", lastname);
