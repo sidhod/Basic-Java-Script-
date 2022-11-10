0fa131a95e4cfd980c9cc63e8f370e0649fd9fdf
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:16:25 +0530

    Add:Examples No 3 Of Async Await

--- asyncAwaitExamples.js
+++ asyncAwaitExamples.js
@@ -51,3 +51,37 @@ async function asyncCall1() {
 //Call Function
 asyncCall1();
 
+
+// Example of Async Await No 3
+//add fuction
+function checkLogin(userName, password) {
+    if (userName == "Sid" && password == "pass@123") {
+        console.log("Welcome Sid");
+        console.log("Login Successful");
+    }
+    else {
+        console.log("Invalid Login!!")
+    }
+
+}
+// timeout for 3 sec
+function login(userName, password) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(checkLogin(userName, password));
+        }, 3000);
+    });
+}
+
+//async fuction
+async function asyncCall2() {
+    const prompt = require('prompt-sync')();
+    let userName = prompt("Enter Your User Name: ");
+    let password = prompt("Enter Your Password: ");
+    console.log("Wait....");
+    await login(userName, password);
+    console.log("End");
+
+}
+//Call Function
+asyncCall2();
