f3c7b239943282e8cf9e314a95f81b2526f702fa
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:07:29 +0530

    Add:Examples No 5 Of Promise

--- promiseExamples.js
+++ promiseExamples.js
@@ -92,3 +92,33 @@ let demo3 = () => {
 demo3()
     .then()
     .catch((error) => console.log("Invalid", error));
+
+
+
+// Example of Promise No 5
+function display(username) {
+    return "Welcome " + username;
+}
+function error() {
+    return "Login Fails";
+}
+let demo4 = () => {
+    return new Promise((resolve, reject) => {
+        const prompt = require('prompt-sync')();
+        let userName = prompt("Enter Your User Name: ");
+        let password = prompt("Enter Your Password: ");
+        if (userName == "Sidhodhan" && password == "pass123") {
+            resolve(display(userName));
+        }
+        else {
+            reject(error())
+        }
+
+    });
+}
+demo4()
+    .then((result) => {
+        console.log(result);
+        console.log("Login Successfully");
+    })
+    .catch((error) => console.log("Invalid", error));
