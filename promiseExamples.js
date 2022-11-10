26db2cbb5a885e9d5abc71a963d9cdac4d83a395
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:05:02 +0530

    Add:Examples No 3 Of Promise

--- promiseExamples.js
+++ promiseExamples.js
@@ -34,3 +34,28 @@ let demo1 = () => {
 demo1()
     .then((value) => console.log("after resolve-->", value))
     .catch((error) => console.log("after reject-->", error));
+
+// Example of Promise No 3
+function firstname(firstName) {
+    return firstName
+}
+function error() {
+    return "Name is null";
+}
+let demo2 = () => {
+    return new Promise((resolve, reject) => {
+        const prompt = require('prompt-sync')();
+        let firstName = prompt("Enter Your Fist Name: ");
+        if (firstName == "") {
+            reject(error());
+        }
+        else {
+            resolve(firstname(firstName));
+
+        }
+
+    });
+}
+demo2()
+    .then((value) => console.log("Hi guys, My name is" + value))
+    .catch((error) => console.log("Invalid", error));
