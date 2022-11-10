77c43e1bd82b7f8d771587f9605b5d45063d406d
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:15:17 +0530

    Add:Examples No 2 Of Async Await

--- asyncAwaitExamples.js
+++ asyncAwaitExamples.js
@@ -19,3 +19,35 @@ async function asyncCall() {
 
 }
 asyncCall();
+
+// Example of Async Await
+//add fuction
+function search(firstName) {
+    const cars = ["Sid", "Sai", "Sidhodhan"];
+    for (let i = 0; i <= 3; i++) {
+        if (cars[i] == firstName) {
+            console.log(firstName + " Is Found In Record");
+            break;
+        }
+    }
+}
+// timeout for 3 sec
+function searchNameInArray(firstName) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(search(firstName));
+        }, 1000);
+    });
+}
+
+//async fuction
+async function asyncCall1() {
+    const prompt = require('prompt-sync')();
+    let firstName = prompt("Enter Your Fist Name: ");
+    console.log("Searching....");
+    await searchNameInArray(firstName);
+
+}
+//Call Function
+asyncCall1();
+
