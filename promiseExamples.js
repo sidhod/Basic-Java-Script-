5e52530f453f0cd463f443a46c18df3a16d2e036
unknown <sidhodhank2018@gmail.com>
2022-11-10 22:04:05 +0530

    Add:Examples No 2 Of Promise

--- promiseExamples.js
+++ promiseExamples.js
@@ -12,3 +12,25 @@ const demo = () => {
 demo()
     .then((value) => console.log("after resolve-->", value))
     .catch((error) => console.log("after reject-->", error));
+
+
+// Example of Promise No 2
+function add(a, b) {
+    return a + b;
+}
+function error() {
+    return "You Got Error";
+}
+let demo1 = () => {
+    return new Promise((resolve, reject) => {
+        let flag = false;
+        if (flag == true) {
+            resolve(add(5, 10));
+        } else {
+            reject(error());
+        }
+    });
+}
+demo1()
+    .then((value) => console.log("after resolve-->", value))
+    .catch((error) => console.log("after reject-->", error));
